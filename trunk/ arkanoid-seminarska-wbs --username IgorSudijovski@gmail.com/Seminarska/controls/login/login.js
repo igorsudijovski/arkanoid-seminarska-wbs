53ceb10140e9adf1b16fa878b8c6ea7651666a9b
IgorSudijovski@gmail.com <IgorSudijovski@gmail.com@f297d744-6029-dbab-3659-640e0c297f04>
2012-11-30 07:33:20 +0000

    without ignores

---
+++ trunk/ arkanoid-seminarska-wbs --username IgorSudijovski@gmail.com/Seminarska/controls/login/login.js
@@ -0,0 +1,33 @@
+﻿function loginApp() {
+    document.getElementById("message").innerHTML = "";
+    var btn = document.getElementById("btnSubmit");
+    btn.onclick = submitLogin;
+}
+function submitLogin(e) {
+    var user = document.getElementById("user").value;
+    var pass = document.getElementById("password").value;
+    var checkbox = document.getElementById("checkbox");
+    var post = {
+        user: user,
+        pass:pass
+    }
+    WinJS.xhr({
+        type: "post",
+        url: host + 'signIn.php',
+        data: formatParams(post),
+        headers: { "Content-type": "application/x-www-form-urlencoded" },
+    }).done(function completed(request) {
+        document.getElementById("message").innerHTML = "";
+        var a = JSON.parse(request.responseText);
+        if (a.error != null) {
+            document.getElementById("message").innerHTML = a.error;
+        }
+    }, 
+        function error(request) {
+            document.getElementById("message").innerHTML = request.statusText;
+        }, 
+        function progress(request) {
+            document.getElementById("message").innerHTML = "processing...";
+        });
+
+}
