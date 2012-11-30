da52eadbbb0ae9f2a0a473d75b49a6a92609d1cb
IgorSudijovski@gmail.com <IgorSudijovski@gmail.com@f297d744-6029-dbab-3659-640e0c297f04>
2012-11-30 01:57:57 +0000

    Arknoid stably version

---
+++  arkanoid-seminarska-wbs --username IgorSudijovski@gmail.com/Seminarska/bin/Release/AppX/js/ball.js
@@ -0,0 +1,82 @@
+﻿function Ball() {
+    this.x = window.event.clientX;
+    this.y = 500;
+    this.dx = 1;
+    this.dy = 1;
+    this.radius = 15;
+    this.speed = 4;
+    this.basicspeed = Math.sqrt(2);
+    this.color = "#FF0000";
+    this.treshHold = 1;
+    this.again = -1;
+    this.init = function (left) {
+        this.touchBase(Math.sqrt(2)/2, left);
+    }
+    this.touchWalls = function (h, w) {
+        if ((this.x + this.radius + this.dx >= w) || (this.x - this.radius + this.dx <= 0)) {
+            this.again = -1;
+            this.touchLeftOrRightWall();
+        }
+        if ((this.y + this.radius + this.dy >= h) || (this.y - this.radius + this.dy <= 0)) {
+            this.again = -1;
+            this.touchBottomOrTopWall();
+        }
+
+    }
+    this.gameOver = function (baseY, context,timer) {
+        if (this.y >= baseY) {
+            clearInterval(timer);
+            document.onmousemove = null;
+            context.font = "bold 100px sans-serif";
+            context.fillStyle = "FFFFFF";
+            context.fillText("Game Over", screen.height / 2, (screen.width * 0.8)/2);
+        }
+
+    }
+    this.touchBase = function (corY, left) {
+        var newspeed = this.basicspeed * this.speed;
+        this.dy = corY * newspeed;
+        this.dx = Math.sqrt((newspeed * newspeed) - (this.dy * this.dy));
+        if (this.dy >= newspeed) this.dx = 0;
+        this.dy *=-1;
+        if(left){
+            this.dx*=-1;
+        }
+        this.again = -1;
+    }
+    this.touchLeftOrRightWall = function () {
+        this.dx *= -1;
+    }
+    this.touchBottomOrTopWall = function () {
+        this.dy *= -1;
+    }
+    this.checkTouchBase = function (base) {
+        if (this.y + this.radius + this.dy < base.y) return false;
+        if ((this.x >= base.x) && (this.x < (base.x + base.length))) {
+            if (this.x + this.treshHold > ((base.x + base.length / 2)) && this.x - this.treshHold < ((base.x + base.length / 2))) {
+                this.touchBase(1, true);
+            } else if (this.x < ((base.x + base.length/2))) {
+                corY = (2 * (this.x - base.x)) / (base.length);
+                if (corY < 0.2) corY = 0.2;
+                this.touchBase(corY, true);
+            } else {
+                corY = (2 * ((base.x + base.length) - this.x)) / (base.length);
+                if (corY < 0.2) cory = 0.2;
+                this.touchBase(corY, false);
+            }
+            return true;
+        }
+        return false;
+    }
+    this.drawBall = function (contex) {
+        contex.beginPath();
+        contex.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, true);
+        contex.fillStyle = this.color;
+        contex.fill();
+        contex.closePath();
+    }
+    this.moveBall = function () {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
---
+++  arkanoid-seminarska-wbs --username IgorSudijovski@gmail.com/Seminarska/bin/Release/AppX/js/test.js
@@ -0,0 +1,118 @@
+﻿var player;
+var canvas;
+var con;
+var ball;
+var base;
+var map;
+var timer;
+var curr = 0;
+var counter;
+var mapstr;
+var file;
+var mapp = null;
+function count() {
+    var days_i = "000" + curr;
+    curr++;
+    change("minutesup", days_i.charAt(days_i.length - 1), 10);
+    change("minutesdown", days_i.charAt(days_i.length - 1), 50);
+    change("seconds2up", days_i.charAt(days_i.length - 2), 10);
+    change("seconds2down", days_i.charAt(days_i.length - 2), 100);
+    change("seconds1up", days_i.charAt(days_i.length - 3), 10);
+    change("seconds1down", days_i.charAt(days_i.length - 3), 150);
+    if (days_i.charAt(days_i.length - 2) == '5' && days_i.charAt(days_i.length - 1) == '9') {
+        curr += 40;
+    }
+
+    counter = setTimeout(count, 1000);
+}
+
+function countEnd() {
+    days_i = "000" + curr;
+    change("minutesup", days_i.charAt(days_i.length - 1), 10);
+    change("minutesdown", days_i.charAt(days_i.length - 1), 50);
+    change("seconds2up", days_i.charAt(days_i.length - 2), 10);
+    change("seconds2down", days_i.charAt(days_i.length - 2), 100);
+    change("seconds1up", days_i.charAt(days_i.length - 3), 10);
+    change("seconds1down", days_i.charAt(days_i.length - 3), 150);
+    if (curr == player.endTime) {
+        clearTimeout(counter);
+        player.showGameOver(con);
+        return;
+    }
+        
+    if (days_i.charAt(days_i.length - 2) == '5' && days_i.charAt(days_i.length - 1) == '9') {
+        curr += 40;
+    }
+    curr++;
+    player.points += 5;
+    player.update();
+    counter = setTimeout(countEnd, 1);
+}
+
+function change(h, val, delay) {
+    setTimeout(function () {
+        document.getElementById(h).innerText = val;
+    }, delay);
+}
+function init() {
+    file = setInterval(getFile, 100);
+    mapp = new mapTest();
+    mapp.test();
+    mapstr = mapp.mapstr;
+    canvas = document.getElementById('myCanvas');
+    con = canvas.getContext('2d');
+    var odocanvas = document.getElementById('score');
+    odocon = odocanvas.getContext('2d');
+    canvas.height = screen.height;
+    canvas.width = screen.width - 300;
+    player = new Player();
+    player.init(canvas.height, canvas.width, odocon);
+    document.getElementById('PlayerName').innerText = player.name;
+    count();
+    //ball = new Ball();
+    //base = new Base();
+    //ball.init();
+    //base.y = canvas.height * 0.9;
+    //base.x = window.event.clientX;
+    //map = new Map();
+    //map.init(canvas.width);
+    //document.onmousemove = moveBase;
+   // timer = setInterval(draw, 10);
+}
+function draw() {
+    con.clearRect(0, 0, canvas.width, canvas.height);
+    player.checkBallsTouchBase();
+    player.moveBalls();
+    player.drawBalls(con);
+    player.base.drawBase(con);
+    player.ballsTouchWalls(canvas.height, canvas.width);
+    player.ballsTouchElements();
+    player.map.draw(con);
+    player.gameOver(con,counter,curr);
+    /*ball.checkTouchBase(base);
+    ball.moveBall();
+    ball.drawBall(con);
+    base.drawBase(con);
+    ball.touchWalls(canvas.height, canvas.width);
+    //ball.gameOver(base.y, con, timer);
+    map.touchElement(ball);
+    map.draw(con);*/
+}
+
+function moveBase(e) {
+    con.clearRect(player.base.x, player.base.y - 1, player.base.length, player.base.height + 2);
+    player.moveBase(e.x,canvas.width);
+    player.base.drawBase(con);
+}
+
+function addBall() {
+    player.addBall();
+}
+function update() {
+    player.update();
+}
+function getFile() {
+    if (mapstr != null) {
+        clearInterval(file);
+    }
+}
